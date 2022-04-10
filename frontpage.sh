c3134f98e43d534d47b5c8fa1c096306ce3de65f
Jason Park <jason.s.park@gmail.com>
2022-04-10 16:02:37 +0900

    fix logic to not dl nyt if todays jpg exists

--- frontpage.sh
+++ frontpage.sh
@@ -16,26 +16,27 @@ yesterjpg="${pathname}${yestername}.jpg"
 
 displayjpg="$todayjpg"
 
-wget "https://static01.nyt.com/images/$todaystring/nytfrontpage/scan.pdf" -O "$todypdf"
-if [ $? -ne 0 ]; then
-  # does not exist, display yesterday's jpg
-  # check if yesterdays jpg exists
-  if [ -f "$yesterjpg" ]; then
-    displayjpg="$yesterjpg"
-    echo "defaulting $yesterjpg"
-  else
-    #get yesterdays pdf
-    wget "https://static01.nyt.com/images/$yesterstring/nytfrontpage/scan.pdf" -O "$yesterpdf"
-    convert -density $PDFDENSITY "$yesterpdf" -quality $JPGQUALITY "$yesterjpg"
-    echo "retrieved $yesterjpg"
-  fi
-  displayjpg="$yesterjpg"
-  echo "displaying $yesterjpg"
+# if todays jpg already exists display that
+if [ -f "$todayjpg" ]; then
+  echo "$todayjpg exists"
 else
-  # if todays jpg already exists display that
-  if [ -f "$todayjpg" ]; then
-    echo "$todayjpg exists"
+  wget "https://static01.nyt.com/images/$todaystring/nytfrontpage/scan.pdf" -O "$todaypdf"
+  if [ $? -ne 0 ]; then
+    # does not exist, display yesterday's jpg
+    # check if yesterdays jpg exists
+    if [ -f "$yesterjpg" ]; then
+      displayjpg="$yesterjpg"
+      echo "defaulting $yesterjpg"
+    else
+      #get yesterdays pdf
+      wget "https://static01.nyt.com/images/$yesterstring/nytfrontpage/scan.pdf" -O "$yesterpdf"
+      convert -density $PDFDENSITY "$yesterpdf" -quality $JPGQUALITY "$yesterjpg"
+      echo "retrieved $yesterjpg"
+    fi
+    displayjpg="$yesterjpg"
+    echo "displaying $yesterjpg"
   else
+    # was able to download today's nyt
     rm "$yesterjpg" "$yesterpdf"
     convert -density $PDFDENSITY "$todaypdf" -quality $JPGQUALITY "$todayjpg"
     echo "retrieved $todayjpg"
